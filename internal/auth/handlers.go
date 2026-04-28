4aed9aaef461b85bc5d63c7a7a15ada87f3302f6
Georgii <georg.argunov@gmail.com>
2026-04-29 00:18:20 +0700

    feature: add login handler

--- internal/auth/handlers.go
+++ internal/auth/handlers.go
@@ -19,21 +19,21 @@ func NewAuthHandler(ctx context.Context, authservice *Service) *AuthHandler {
 }
 
 func (h *AuthHandler) RegisterUser(w http.ResponseWriter, r *http.Request){
-	var new_user dto.UserDTO
-	if err := json.NewDecoder(r.Body).Decode(&new_user); err != nil {
+	var userDto dto.UserDTO
+	if err := json.NewDecoder(r.Body).Decode(&userDto); err != nil {
 		errDTO := dto.NewErrorDTO(err)
 		http.Error(w, errDTO.ToString(), http.StatusBadRequest)
 		return
 	}
 
-	if !new_user.Validate() {
+	if !userDto.Validate() {
 		http.Error(w, "Invalid email or password", http.StatusBadRequest)
 		return
 	}
 
 	user := User {
-		Email: new_user.Email,
-		Password: new_user.Password,
+		Email: userDto.Email,
+		Password: userDto.Password,
 	}
 
 	if err := h.authService.Register(r.Context(), user); err != nil {
@@ -61,3 +61,47 @@ func (h *AuthHandler) RegisterUser(w http.ResponseWriter, r *http.Request){
 	}
 }
 
+func (h *AuthHandler) LoginUser(w http.ResponseWriter, r *http.Request){
+	var userDto dto.UserDTO
+	if err := json.NewDecoder(r.Body).Decode(&userDto); err != nil {
+		errDTO := dto.NewErrorDTO(err)
+		http.Error(w, errDTO.ToString(), http.StatusBadRequest)
+		return
+	}
+
+	if !userDto.Validate() {
+		http.Error(w, "Invalid email or password", http.StatusBadRequest)
+		return
+	}
+
+	user := User {
+		Email: userDto.Email,
+		Password: userDto.Password,
+	}
+
+	token, err := h.authService.Login(r.Context(), user)
+
+	if err != nil {
+		errDTO := dto.NewErrorDTO(err)
+		if errors.Is(err, ErrUserNotFound) || errors.Is(err, ErrWrongPassword){
+			http.Error(w, errDTO.ToString(), http.StatusUnauthorized)
+			return
+		} else {
+			http.Error(w, errDTO.ToString(), http.StatusInternalServerError)
+			return
+		}
+	}
+
+	w.WriteHeader(http.StatusOK)
+
+	b, err := json.MarshalIndent(token, "", "\t")
+	if err != nil {
+		errDTO := dto.NewErrorDTO(err)
+		http.Error(w, errDTO.ToString(), http.StatusInternalServerError)
+		return
+	}
+
+	if _, err := w.Write(b); err != nil {
+		return
+	}
+}
