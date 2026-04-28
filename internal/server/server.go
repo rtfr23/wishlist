bb7e4db2925ad2b3c7b09aca8602fec4d496d97a
Georgii <georg.argunov@gmail.com>
2026-04-28 16:25:59 +0700

    feature: add initialization http server wrapper

---
+++ internal/server/server.go
@@ -0,0 +1,70 @@
+package server
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type HTTPServer struct {
+	httpHandler *HTTPHandlers
+}
+
+type HTTPHandlers struct {
+	/*
+	TODO:
+		auth service
+		wishlist service
+		item service
+	*/
+}
+
+func NewHTTPServer(handler *HTTPHandlers) *HTTPServer{
+	return &HTTPServer {
+		httpHandler: handler,
+	}
+}
+
+func (s *HTTPServer)Start(port string) error {
+	router := mux.NewRouter()
+
+	/*
+	TODO:
+	endpoints
+
+	auth:
+		POST /auth/signin
+		POST /auth/signup
+	
+	wishlist:
+		closed:
+			GET /wishlists
+			GET /wishlists/{id}
+			POST /wishlists
+			PATCH /wishlists/{id}
+			DELETE /wishlists/{id}
+		opened:
+			GET /wishlists/{token}
+
+	item:
+		closed:
+			GET /wishlists/name/items/{id}
+			GET /wishlists/name/items
+			PATCH /wishlists/name/items/{id}
+			POST /wishlists/name/items
+			DELETE /wishlists/name/items/{id}
+		opened:
+			PATCH /wishlists/token/items/{id}
+	*/
+
+	if err := http.ListenAndServe(port, router); err != nil {
+		if errors.Is(err, http.ErrServerClosed) {
+			return nil
+		} else {
+			return err
+		}
+	}
+
+	return nil
+}
