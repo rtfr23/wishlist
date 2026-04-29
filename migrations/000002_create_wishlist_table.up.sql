1e15a8b794a84c98dde184dab453c1376a476fd0
Georgii <georg.argunov@gmail.com>
2026-04-29 14:03:03 +0700

    feature: add migration for wishlists table

---
+++ migrations/000002_create_wishlist_table.up.sql
@@ -0,0 +1,9 @@
+CREATE TABLE wishlists (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    event VARCHAR(256) NOT NULL,
+    description VARCHAR(1024),
+    date TIMESTAMP NOT NULL,
+
+    FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE
+);
