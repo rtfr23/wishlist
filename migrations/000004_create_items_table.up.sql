b4dbbdfe5103fbf414f00de45c2544d95c644ede
Georgii <georg.argunov@gmail.com>
2026-04-30 04:41:09 +0300

    feature: add migrations for items table

---
+++ migrations/000004_create_items_table.up.sql
@@ -0,0 +1,11 @@
+CREATE TABLE items (
+    id SERIAL PRIMARY KEY
+    wishlist_id INTEGER NOT NULL,
+    title VARCHAR(256),
+    description VARCHAR(1024),
+    url VARCHAR(512),
+    priority INT NOT NULL,
+    is_reserved BOOLEAN NOT NULL DEFAULT FALSE,
+
+    FOREIGN KEY (wishlist_id) REFERENCES wishlists(id) ON DELETE CASCADE
+);
